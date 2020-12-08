0d2de0c3c4418556730b3f97b3e891bc4175fec7
Chelsea Senter <chelsea.senter10@gmail.com>
2020-12-08 17:00:12 -0500

    save point, don't want to lose work!

--- PyBank/main.py
+++ PyBank/main.py
@@ -31,25 +31,19 @@ with open(finance_csv) as bankfile:
         month_list.append(row[0])
         amount_list.append(row[1])
 
-
-    print(f"month list: {len(list(month_list))}")
-
-    print(f"amount list: {len(list(amount_list))}")
-
-    print(f"csv file list: {len(list(full_csv))}")
-
+    for row in full_csv:
+        print(row)
 
 ##----DONE---- COUNT TOTAL MONTHS IN DATA
 # get length of rows to get the total months included in the data:
     month_count = len(month_list)
-    print(f"this is the month_count = {month_count}")
+
     
 # ----DONE---- NET CHANGE OF ALL MONTHS OVER THE PERIOD:
 # this will add all profits/losses together for a net
     sum_net= 0
     for row in amount_list:
         sum_net = sum_net + int(row)
-    print(f"The net profits for this period is: {sum_net}")
 
 
 # ## CALCULATE CHANGES OVER THE PERIOD, THEN GET AVERAGE CHANGE
@@ -61,8 +55,6 @@ with open(finance_csv) as bankfile:
         diff_list.append(int(row) - prev_month)
         prev_month = int(row)
     diff_header = diff_list.pop(0)
-
-    print(f"this is the diff_list = {len(list(diff_list))}")
 
 # remember: "header" of diff_list is the first month - 0 (not the "first" difference value we want)
 
@@ -80,30 +72,47 @@ with open(finance_csv) as bankfile:
     for item in diff_list:
         sum_diff = sum_diff + int(item)
     average_diff = sum_diff / month_count
-    print(f"This is the average change over the period: {average_diff}")
 
 # ## GREATEST PROFIT MONTH, AMOUNT
 # # using the list of differences between each month, find the highest number, but check for multiples
     greatest_profit = max(amount_list)
+    gp_index = amount_list.index(greatest_profit)
     # maxi_count = 0
+    # multi_maxi = []
     # for maxi in amount_list:
     #     maxi_count += 1
     # if max_count < 1:
     #     print(f"The greatest loss occured during the month of {reference the month_list item here}: {greatest_profit}")
+    # else:
+    #     print(f"There were multiple months that had the greatest profit. These months are:")
+    #     print(f"{multi_maxi}")
 
 # ## GREATEST LOSS MONTH, AMOUNT
 # # using the list of differences between each month, find the lowest number, but check for multiples
     greatest_loss = min(amount_list)
+    gl_index = amount_list.index(greatest_loss)
     # mini_count = 0
+    # multi_mini = []
     # for mini in amount_list:
     #     mini_count += 1
     # if mini_count < 1: 
     #     print(f"The greatest loss occured during the month of {reference the month_list item here}: {greatest_loss}")
     # else:
+    #     for minis in amount_list:
+    #         if minis == greatest_loss:
+    #             multi_mini.append(minis)
     #     print(f"There were multiple months that had the greatest loss. These months are:")
-    #     print(f"{multi_mini_list}")
+    #     for minis in multi_mini:
+    #         print(f"{multi_mini}")
 
 
 
 # ##PRINT RESULTS
 # # print title, -----, Total Months, Total Net, Average Change, 
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {month_count}")
+print(f"Total: ${sum_net}")
+print(f"Average  Change: ${average_diff}")
+print(f"Greatest Increase in Profits: {month_list[gp_index]} (${greatest_profit})")
+print(f"Greatest Decrease in Profits: {month_list[gl_index]} (${greatest_loss})")
