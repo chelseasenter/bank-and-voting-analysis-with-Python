a3f4525c2c828dc6f6412f3de44776cdff7d67bc
Chelsea Senter <chelsea.senter10@gmail.com>
2020-12-08 11:08:50 -0500

    completed the total month count AND the net profits/losses over period- Woo!

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,44 +12,62 @@ finance_csv = os.path.join("Resources","budget_data.csv")
 
 #open the csv file and set the csv.reader function to read it:
 
-with open(finance_csv, encoding='utf-8') as bankfile:
+with open(finance_csv) as bankfile:
     csvreader = csv.reader(bankfile, delimiter=",")
 
+    print(csvreader)
+    print("---- csv is pulling up----")
+# # get rid of the header!
+#     print({csvreader[3][0]})
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
 
-# get rid of the header!
-nix_header = next(csvreader)
-
-
-## COUNT TOTAL MONTHS IN DATA
-# get length of rows to get the total months included in the data:
-month_count = len(list(csvreader))
-
-print(month_count)
-
-
-## NET CHANGE OF ALL MONTHS OVER THE PERIOD:
-# get the sum of all profit/losses over the period:
-net_total = 0
-for month in csvreader: 
-    net_total += month[1]
-
-print(net_total)
+# DONE---------
+#this take the previous row and gets the difference (current month - previous month profit/loss)
+#     next = 0
+#     for row in csvreader:
+#         print(f"MONTH AMOUNT: {row[1]}")
+#         print(f"month amount is {int(row[1])} minus {next} = {int(row[1]) - next}")
+#         next = int(row[1])
+#         print(int(next))
+# DONE^^^^^^^^^
 
 
-## CALCULATE CHANGES OVER THE PERIOD, THEN GET AVERAGE CHANGE
-# find the difference between each month and add that number in a new list:
+# ## COUNT TOTAL MONTHS IN DATA
+# # get length of rows to get the total months included in the data:
+#     month_count = len(list(csvreader))
 
-for month in csvreader:
-    prev_month = 0
+#     print(month_count)
+#     print("----month count ^ ----")
 
+# ## NET CHANGE OF ALL MONTHS OVER THE PERIOD:
+#this will add all profits/losses together for a net
+    sum = 0
+    for row in csvreader:
+        sum += int(row[1])
+    print(f"The net profits for this period is: {sum}")
 
-# find the average of all changes over the period:
+# ## CALCULATE CHANGES OVER THE PERIOD, THEN GET AVERAGE CHANGE
+# # find the difference between each month and add that number in a new list:
+#     diff_list = []
 
-## GREATEST PROFIT MONTH, AMOUNT
-# using the list of differences between each month, find the highest number, but check for multiples
+#     for row in csvreader:
+#         print(row)
+#     #set previous month to 0
+#     #     prev_month = 0
+#     # # #add the change from the previous month and the current row (this month) to the diff_list
+#     # # diff_list.append(prev_month + row[1])
+#     #     print(f"{prev_month}")
+#     #     prev_month = row[1]
+#     print("----diff list? ^ ----")
 
-## GREATEST LOSS MONTH, AMOUNT
-# using the list of differences between each month, find the lowest number, but check for multiples
+# # find the average of all changes over the period:
 
-##PRINT RESULTS
-# print title, -----, Total Months, Total Net, Average Change, 
+# ## GREATEST PROFIT MONTH, AMOUNT
+# # using the list of differences between each month, find the highest number, but check for multiples
+
+# ## GREATEST LOSS MONTH, AMOUNT
+# # using the list of differences between each month, find the lowest number, but check for multiples
+
+# ##PRINT RESULTS
+# # print title, -----, Total Months, Total Net, Average Change, 
